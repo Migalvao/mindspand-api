f25f427e184f933fb275b65ae3119c8c6071002b
alexpinho <arp2323arp@gmail.com>
2021-08-16 11:50:49 +0100

    cards categories organized

--- app/javascript/Pages/components/CardCategory.jsx
+++ app/javascript/Pages/components/CardCategory.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const CardCategory = ({ categoryData }) => {
   console.log(categoryData);
-  return <div className="card-category">{categoryData.name}</div>;
+  return (
+    <div
+      className="card-category"
+      style={{ "--colorCategory": `#${categoryData.color}` }}
+    >
+      {categoryData.name}
+    </div>
+  );
 };
 
 export default CardCategory;
