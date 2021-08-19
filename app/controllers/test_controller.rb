e1bfe67151e5e9bb739dbe06400d8b98e2e67611
Miguel Galvao <miguelgalvao@live.com.pt>
2021-08-19 18:23:27 +0100

    removed unecessary class

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -19,23 +19,3 @@ class TestController < ApplicationController
     end
   end
 end
-
-class TestController < ApplicationController
-  include AuthenticationConcern
-
-  def home
-    if @current_user
-      render(inertia: 'Home', props: { "current_user": @current_user.as_json })
-    else
-      redirect_to '/login'
-    end
-  end
-
-  def index
-    puts SKILLS
-    # puts SKILLS["1"]
-
-    res = { 'success' => 'Article was successfully deleted' }
-    render(json: res)
-  end
-end
