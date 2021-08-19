2e758df905e6a62063ffdc469f184541b9d83569
alexpinho <arp2323arp@gmail.com>
2021-08-19 12:18:39 +0100

    BEM formating social-card

--- app/javascript/Pages/components/SocialCard.jsx
+++ app/javascript/Pages/components/SocialCard.jsx
@@ -5,14 +5,14 @@ const SocialCard = ({ newClassData }) => {
   const link = newClassData.teacher.avatar.url + "?tr=w-250,h-300";
 
   return (
-    <div className="cardSocial">
-      <img src={link} alt="photo" className="cardSocial-img" />
-      <div className="cardSocial-filter"></div>
-      <div className="cardSocial-chip-new">New</div>
-      <div className="cardSocial-dif-level">{newClassData.difficulty}</div>
-      <h3 className="cardSocial-skill-name">{newClassData.skill.name}</h3>
-      <h4 className="cardSocial-class-title">{newClassData.title}</h4>
-      <h5 className="cardSocial-teacher-name">{newClassData.teacher.name}</h5>
+    <div className="card-social">
+      <img src={link} alt="photo" className="card-social-img" />
+      <div className="card-social-filter"></div>
+      <div className="card-social-chip-new">New</div>
+      <div className="card-social-dif-level">{newClassData.difficulty}</div>
+      <h3 className="card-social-skill-name">{newClassData.skill.name}</h3>
+      <h4 className="card-social-class-title">{newClassData.title}</h4>
+      <h5 className="card-social-teacher-name">{newClassData.teacher.name}</h5>
     </div>
   );
 };
