9b51e17637b26e6bb78eaa2a2dbca3c111b6c5b9
alexpinho <arp2323arp@gmail.com>
2021-08-31 17:49:05 +0100

    nonsense corrections

--- app/javascript/Pages/Notifications.jsx
+++ app/javascript/Pages/Notifications.jsx
@@ -1,147 +1,34 @@
 import React from "react";
-import axios from "axios";
 import PropTypes from "prop-types";
-import { Inertia } from "@inertiajs/inertia";
-import { useState } from "react";
-import { IoIosArrowBack } from "react-icons/io";
+import { Head, Link } from "@inertiajs/inertia-react";
 import Layout from "./Layout";
 
 export default function Welcome(props) {
-  const [notifications, setNotifications] = useState(props.notifications);
+  const Notifications_list = (props) => {
+    console.log(props.notifications);
+    const notificationsList = props.notifications.map((n, index) => (
+      <li key={index}>
+        id: {n.id}
+        text: {n.text}
+        notification_type: {n.notification_type}
+      </li>
+    ));
 
-  const requestOnClick = (match_id, answer) => {
-    const url = `/api/match_request/${match_id}`;
-    axios
-      .put(url, {
-        status: answer,
-        headers: window.defaultHeaders,
-      })
-      .then(() => {
-        //update notifications
-        axios
-          .get("/api/notifications")
-          .then((response) => {
-            setNotifications(response.data.notifications);
-          })
-          .catch((error) => {
-            alert(error.response.data.error);
-          });
-      })
-      .catch((error) => alert(error.response.data.error));
-  };
-
-  const reviewOnClick = () => {
-    alert("Review connection!");
-  };
-
-  const avatar = (image) => {
-    return `${image}?tr=w-60,h-60,r-max`;
+    return <ul>{notificationsList}</ul>;
   };
 
   return (
     <Layout current_user={props.current_user}>
-      <div>
-        <IoIosArrowBack
-          onClick={() => {
-            Inertia.get("/home");
-          }}
-        />
-        <h1>Activity</h1>
-      </div>
+      <Head title="Welcome" />
+      <h1>Notifications</h1>
+      <p>Unread notifications</p>
+      <Notifications_list notifications={props.notifications.unread} />
+      <p>Read notifications</p>
+      <Notifications_list notifications={props.notifications.read} />
 
-      <div>
-        <h2>Classes requests</h2>
-        {notifications.requests.map((notification, index) => {
-          return (
-            <div key={index}>
-              <img src={avatar(notification.match.student.avatar.url)} />
-              <p>{notification.text}</p>
-              <p>{notification.match.skill_class.title}</p>
-              <button
-                onClick={() =>
-                  requestOnClick(notification.match.id, "accepted")
-                }
-              >
-                Confirm
-              </button>
-              <button
-                onClick={() => requestOnClick(notification.match.id, "refused")}
-              >
-                Remove
-              </button>
-            </div>
-          );
-        })}
-      </div>
-
-      <p>_________________________</p>
-      {notifications.regular.map((notification, index) => {
-        if (notification.notification_type == "match_accepted") {
-          // There should be a "Talk" button if the connection is still in progress
-          return (
-            <div key={index}>
-              <img
-                src={avatar(notification.match.skill_class.teacher.avatar.url)}
-              />
-              ACCEPTED
-              <p>{notification.text}</p>
-              <p>{notification.match.skill_class.title}</p>
-              {notification.match.connection.class_status == "in_progress" ? (
-                <button
-                  onClick={() =>
-                    Inertia.get(
-                      `/connections/${notification.match.connection.id}`
-                    )
-                  }
-                >
-                  Talk
-                </button>
-              ) : null}
-            </div>
-          );
-        } else if (notification.notification_type == "connection_closed") {
-          return (
-            <div
-              key={index}
-              onClick={
-                notification.match.connection.class_status == "given" &&
-                !notification.read
-                  ? () => reviewOnClick()
-                  : null
-              }
-            >
-              {notification.match.connection.person_closed_connection ==
-              "student_closed" ? (
-                <img src={avatar(notification.match.student.avatar.url)} />
-              ) : (
-                <img
-                  src={avatar(
-                    notification.match.skill_class.teacher.avatar.url
-                  )}
-                />
-              )}
-              {notification.match.connection.class_status == "given"
-                ? "CLASS GIVEN:"
-                : "CLASS CANCELLED"}
-              <p>{notification.text}</p>
-            </div>
-          );
-        } else {
-          // match_refused or previously accepted match, only text
-          return (
-            <div key={index}>
-              <img
-                src={avatar(
-                  notification.notification_type == "match_denied"
-                    ? notification.match.skill_class.teacher.avatar.url
-                    : notification.match.student.avatar.url
-                )}
-              />
-              <p>{notification.text}</p>
-            </div>
-          );
-        }
-      })}
+      <Link href="/home" headers={Window.defaultHeaders} as="button">
+        Home
+      </Link>
     </Layout>
   );
 }
