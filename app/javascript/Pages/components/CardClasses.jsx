e41804f91f42aaeb8c178dad4b7d7c38ea68ac1f
Miguel Galvao <miguelgalvao@live.com.pt>
2021-09-01 15:57:07 +0100

    add onclick navigation for class cards

--- app/javascript/Pages/components/CardClasses.jsx
+++ app/javascript/Pages/components/CardClasses.jsx
@@ -1,38 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Inertia } from "@inertiajs/inertia";
 
 const CardClasses = ({ classesData }) => {
-  const link = classesData.teacher.avatar.url + "?tr=w-250,h-300";
+ const link =
+  classesData.teacher.avatar.url + "?tr=w-250,h-300";
 
-  return (
-    <div className="card-classes">
-      <img src={link} alt="photo" className="card-class-img" />
-      <div className="card-class-filter"></div>
-      <div className="card-class-chip-new">New</div>
-      <div className="card-class-dif-level">{classesData.difficulty}</div>
-      <div className="card-class-info">
-        <h5
-          className="card-class-skill-name"
-          style={{ "--colorCategory": `#${classesData.skill.category.color}` }}
-        >
-          {classesData.skill.name}
-        </h5>
-        <h4 className="card-class-duration">
-          {classesData.no_classes} L&nbsp;
-          <span className="card-class-duration-time">
-            {classesData.class_duration}min
-          </span>
-        </h4>
+ return (
+  <div
+   className="card-classes"
+   onClick={() => Inertia.get(`/classes/${classesData.id}`)}
+  >
+   <img src={link} alt="photo" className="card-class-img" />
+   <div className="card-class-filter"></div>
+   <div className="card-class-chip-new">New</div>
+   <div className="card-class-dif-level">
+    {classesData.difficulty}
+   </div>
+   <div className="card-class-info">
+    <h5
+     className="card-class-skill-name"
+     style={{
+      "--colorCategory": `#${classesData.skill.category.color}`,
+     }}
+    >
+     {classesData.skill.name}
+    </h5>
+    <h4 className="card-class-duration">
+     {classesData.no_classes} L&nbsp;
+     <span className="card-class-duration-time">
+      {classesData.class_duration}min
+     </span>
+    </h4>
 
-        <h2 className="card-class-title">{classesData.title}</h2>
-        <h3 className="card-class-teacher-name">{classesData.teacher.name}</h3>
-      </div>
-    </div>
-  );
+    <h2 className="card-class-title">
+     {classesData.title}
+    </h2>
+    <h3 className="card-class-teacher-name">
+     {classesData.teacher.name}
+    </h3>
+   </div>
+  </div>
+ );
 };
 
 CardClasses.propTypes = {
-  classesData: PropTypes.object.isRequired,
+ classesData: PropTypes.object.isRequired,
 };
 
 export default CardClasses;
--- app/javascript/Pages/components/CardPopular.jsx
+++ app/javascript/Pages/components/CardPopular.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Inertia } from "@inertiajs/inertia";
 
 const CardPopular = ({ popularClassData }) => {
-  const link = popularClassData.teacher.avatar.url + "?tr=w-250,h-300";
+ const link =
+  popularClassData.teacher.avatar.url + "?tr=w-250,h-300";
 
-  return (
-    <div className="media">
-      <img src={link} alt="photo" className="media-figure" />
+ return (
+  <div
+   className="media"
+   onClick={() =>
+    Inertia.get(`/classes/${popularClassData.id}`)
+   }
+  >
+   <img src={link} alt="photo" className="media-figure" />
 
-      <div className="media-body">
-        <div className="media-body-info">
-          <h1 className="media-teacher">{popularClassData.teacher.name}</h1>
-          <h2 className="media-skill">{popularClassData.skill.name}</h2>
-        </div>
-      </div>
+   <div className="media-body">
+    <div className="media-body-info">
+     <h1 className="media-teacher">
+      {popularClassData.teacher.name}
+     </h1>
+     <h2 className="media-skill">
+      {popularClassData.skill.name}
+     </h2>
     </div>
-  );
+   </div>
+  </div>
+ );
 };
 
 CardPopular.propTypes = {
-  popularClassData: PropTypes.object.isRequired,
+ popularClassData: PropTypes.object.isRequired,
 };
 
 export default CardPopular;
