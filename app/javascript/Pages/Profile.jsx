7fea501a8e591d787ac3aa51c1b6d6b743537d52
alexpinho <arp2323arp@gmail.com>
2021-09-02 17:25:55 +0100

    profile info + styling

--- app/javascript/Pages/Profile.jsx
+++ app/javascript/Pages/Profile.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "@inertiajs/inertia-react";
+import { useState } from "react";
 import Layout from "./Layout";
 
 export default function Profile(props) {
+  const [click, setClick] = useState("aboutme");
+
+  const handleClick = (newState) => setClick(newState);
+
+  const avatar = (image) => {
+    return `${image}?tr=,r-max`;
+  };
+
   const EditButton = () => {
     if (props.can_edit) {
       return (
         <Link
+          className="btn-edit-profile"
           href={props.user.id + "/edit"}
           headers={window.defaultHeaders}
           as="button"
         >
-          Edit
+          Edit Profile
         </Link>
       );
     }
@@ -21,18 +31,59 @@ export default function Profile(props) {
 
   return (
     <Layout current_user={props.user}>
-      <p>Id: {props.user.id}</p>
-      <p>Name: {props.user.name}</p>
-      <p>Username: {props.user.username}</p>
-      <p>Rating: {props.user.rating}</p>
-      <p>Description: {props.user.description}</p>
-      <p>Can edit?: {props.can_edit.toString()}</p>
-      <EditButton />
-      <br></br>
+      <div className="profile-info">
+        <img
+          src={avatar(props.user.avatar.url)}
+          alt="Avatar"
+          className="profile-info-avatar"
+        />
+        <p className="profile-info-username">{props.user.username}</p>
+        <p className="profile-info-name">{props.user.name}</p>
+        <p className="profile-info-rating">{props.user.rating} stars</p>
+        <EditButton />
+      </div>
 
-      <Link href="/home" headers={window.defaultHeaders} as="button">
-        Home
-      </Link>
+      <div>
+        <ul className="profile-menu">
+          <li
+            className={
+              click == "aboutme"
+                ? "profile-menu-btn active"
+                : "profile-menu-btn"
+            }
+            onClick={() => handleClick("aboutme")}
+          >
+            About me
+          </li>
+          <li
+            className={
+              click == "skills" ? "profile-menu-btn active" : "profile-menu-btn"
+            }
+            onClick={() => handleClick("skills")}
+          >
+            Skills
+          </li>
+          <li
+            className={
+              click == "history"
+                ? "profile-menu-btn active"
+                : "profile-menu-btn"
+            }
+            onClick={() => handleClick("history")}
+          >
+            History
+          </li>
+        </ul>
+      </div>
+      <div
+        className={
+          click == "aboutme" ? "profile-aboutme" : "profile-aboutme off"
+        }
+      >
+        <h1 className="home-title">About me</h1>
+        <p className="profile-aboutme-text">{props.user.description}</p>
+        <br></br>
+      </div>
     </Layout>
   );
 }
