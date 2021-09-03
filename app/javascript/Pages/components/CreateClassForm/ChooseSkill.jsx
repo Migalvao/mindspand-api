4c93a45660a90758bbe067a2b725bdb411f4744d
alexpinho <arp2323arp@gmail.com>
2021-09-03 14:38:16 +0100

    setup

--- app/javascript/Pages/components/CreateClassForm/ChooseSkill.jsx
+++ app/javascript/Pages/components/CreateClassForm/ChooseSkill.jsx
@@ -4,106 +4,105 @@ import axios from "axios";
 import ButtonFilter from "../ButtonFilter";
 
 export default class ChooseSkill extends Component {
- constructor(props) {
-  super(props);
-  this.handleChange = props.handleChange;
-  this.nextStep = props.nextStep;
-  this.previousStep = props.previousStep;
+  constructor(props) {
+    super(props);
+    this.handleChange = props.handleChange;
+    this.nextStep = props.nextStep;
+    this.previousStep = props.previousStep;
 
-  this.state = {
-   error: "",
-   categories: [],
-   skills: [],
-  };
- }
+    this.state = {
+      error: "",
+      categories: [],
+      skills: [],
+    };
+  }
 
- componentDidMount() {
-  // same as use effect
-  axios.get("/api/skills").then((response) => {
-   const categories = response.data;
-   let skills = [];
-   if (this.props.skill.id) {
-    categories.forEach((c) => {
-     if (this.props.skill.category_id == c.id) {
-      skills = c.skills;
-      return;
-     }
+  componentDidMount() {
+    // same as use effect
+    axios.get("/api/skills").then((response) => {
+      const categories = response.data;
+      let skills = [];
+      if (this.props.skill.id) {
+        categories.forEach((c) => {
+          if (this.props.skill.category_id == c.id) {
+            skills = c.skills;
+            return;
+          }
+        });
+      }
+      this.setState({
+        categories: categories,
+        skills: skills,
+      });
     });
-   }
-   this.setState({
-    categories: categories,
-    skills: skills,
-   });
-  });
- }
+  }
 
- componentDidUpdate() {}
+  componentDidUpdate() {}
 
- render() {
-  return (
-   <div>
-    <br />
-    <br />
-    <div onClick={this.previousStep}>Back</div>
-    Choose Skill
-    {this.state.categories.length ? (
-     this.state.categories.map((c, i) => {
-      return (
-       <ButtonFilter
-        key={i}
-        params={c}
-        onClick={(c) => {
-         this.setState({ skills: c.skills });
-         this.handleChange({
-          skill: { category_id: c.id },
-         });
-        }}
-        id={this.props.skill.category_id}
-       >
-        {c.name}
-       </ButtonFilter>
-      );
-     })
-    ) : (
-     <p>Loading...</p>
-    )}
-    {this.state.skills.map((s, i) => {
-     return (
-      <div
-       key={i}
-       onClick={() =>
-        this.handleChange({
-         skill: {
-          category_id: this.props.skill.category_id,
-          id: s.id,
-          name: s.name,
-         },
-        })
-       }
-      >
-       {s.name +
-        (s.id == this.props.skill.id ? " SELECTED" : "")}
-      </div>
-     );
-    })}
-    <br />
-    <p>{this.state.error}</p>
-    <div
-     onClick={() => {
-      if (this.props.skill.id) this.nextStep();
-      else this.setState({ error: "Skill is mandatory" });
-     }}
-    >
-     Next
-    </div>
-   </div>
-  );
- }
+  render() {
+    return (
+      <main>
+        <br />
+        <br />
+        <div onClick={this.previousStep}>Back</div>
+        Choose Skill
+        {this.state.categories.length ? (
+          this.state.categories.map((c, i) => {
+            return (
+              <ButtonFilter
+                key={i}
+                params={c}
+                onClick={(c) => {
+                  this.setState({ skills: c.skills });
+                  this.handleChange({
+                    skill: { category_id: c.id },
+                  });
+                }}
+                id={this.props.skill.category_id}
+              >
+                {c.name}
+              </ButtonFilter>
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+        {this.state.skills.map((s, i) => {
+          return (
+            <div
+              key={i}
+              onClick={() =>
+                this.handleChange({
+                  skill: {
+                    category_id: this.props.skill.category_id,
+                    id: s.id,
+                    name: s.name,
+                  },
+                })
+              }
+            >
+              {s.name + (s.id == this.props.skill.id ? " SELECTED" : "")}
+            </div>
+          );
+        })}
+        <br />
+        <p>{this.state.error}</p>
+        <div
+          onClick={() => {
+            if (this.props.skill.id) this.nextStep();
+            else this.setState({ error: "Skill is mandatory" });
+          }}
+        >
+          Next
+        </div>
+      </main>
+    );
+  }
 }
 
 ChooseSkill.propTypes = {
- handleChange: PropTypes.func.isRequired,
- previousStep: PropTypes.func.isRequired,
- nextStep: PropTypes.func.isRequired,
- skill: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  previousStep: PropTypes.func.isRequired,
+  nextStep: PropTypes.func.isRequired,
+  skill: PropTypes.object.isRequired,
 };
